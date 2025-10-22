b19a72a343302551b8e9b13202a5be93d0899841
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-22 23:10:24 +0300

    feat(security): add EncryptionUtil

---
+++ GoalTrackerApp/Security/EncryptionUtil.cs
@@ -0,0 +1,17 @@
+namespace GoalTrackerApp.Security
+{
+    public static class EncryptionUtil
+    {
+        public static string Encrypt(string plainText)
+        {
+            var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(plainText);
+            return encryptedPassword;
+        }
+
+        public static bool IsValidPassword(string plainText, string cipherText)
+        {
+            var isValid = BCrypt.Net.BCrypt.Verify(plainText, cipherText);
+            return isValid;
+        }
+    }
+}
