5e1bd229c861662d24de56eff424b8cd15d52528
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-23 13:03:25 +0300

    feat: add exceptions

---
+++ GoalTrackerApp/Exceptions/AppException.cs
@@ -0,0 +1,12 @@
+﻿namespace GoalTrackerApp.Exceptions
+{
+    public abstract class AppException : Exception
+    {
+        public string Code { get; set; }
+
+        public AppException(string code, string message) : base(message)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ GoalTrackerApp/Exceptions/ServerException.cs
@@ -0,0 +1,10 @@
+﻿namespace GoalTrackerApp.Exceptions
+{
+    public class ServerException : AppException
+    {
+        public ServerException(string code, string message)
+            : base(code, message)
+        {
+        }
+    }
+}
