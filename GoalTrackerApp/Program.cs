f0c544f0c02907afe4f2194155fc2f01fe69308d
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-23 10:05:09 +0300

    chore: add automapper, connection string config & serilog config

--- GoalTrackerApp/Program.cs
+++ GoalTrackerApp/Program.cs
@@ -1,3 +1,8 @@
+using GoalTrackerApp.Configuration;
+using GoalTrackerApp.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 namespace GoalTrackerApp;
 
 public class Program
@@ -5,9 +10,16 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
+        var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+        connString = connString!.Replace("{DB_PASS}", Environment.GetEnvironmentVariable("DB_PASS") ?? "");
+        
         // Add services to the container.
-
+        
+        builder.Services.AddDbContext<GoalTrackerApp.Data.GoalTrackerAppDbContext>(options => options.UseSqlServer(connString));
+        builder.Services.AddRepositories();
+        builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MapperConfig>());
+        builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
+        
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
