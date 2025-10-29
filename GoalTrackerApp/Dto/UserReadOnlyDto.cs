e03ddecc46d7ff2d8afc5bddd64007244f85cbd3
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-29 13:21:41 +0200

    feat(dto): add user read only dto

---
+++ GoalTrackerApp/Dto/UserReadOnlyDto.cs
@@ -0,0 +1,12 @@
+namespace GoalTrackerApp.Dto
+{
+    public record UserReadOnlyDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string UserRole { get; set; } = null!;
+    }
+}
