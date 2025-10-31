c940a0625dd2fca2b6a93d5bc0f5164231e3cf44
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-31 10:49:04 +0200

    feat(models): add application user

---
+++ GoalTrackerApp/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+namespace GoalTrackerApp.Models;
+
+public class ApplicationUser
+{ 
+     public int Id { get; set; }
+     public string? Username { get; set; }
+     public string? Email { get; set; }
+}
+
